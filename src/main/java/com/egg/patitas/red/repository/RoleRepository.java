f9a6049aa0d27e74bc49ea332593afc371d2c599
Bruno Redzio <brunoredzio@hotmail.com>
2021-11-29 19:48:22 -0300

    Bruno/Generacion de entidad rol y animal

---
+++ src/main/java/com/egg/patitas/red/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.egg.patitas.red.repository;
+
+import com.egg.patitas.red.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+}
