53c196bcc5a9ee905e3601b922b5d2e108649776
Maximiliano Monje <maxiiigabrielmonje@gmail.com>
2021-11-29 19:48:26 -0300

    maxi/entidad y repositorio Pet

---
+++ src/main/java/com/egg/patitas/red/model/Pet.java
@@ -0,0 +1,48 @@
+package com.egg.patitas.red.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Pet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String photo;
+
+    @Column(nullable = false)
+    private Boolean enabled;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne
+    private Animal animal;
+
+    @Column
+    @CreatedDate
+    private LocalDateTime dateCreated;
+
+    @Column
+    @LastModifiedDate
+    private LocalDateTime dateModified;
+
+
+}
