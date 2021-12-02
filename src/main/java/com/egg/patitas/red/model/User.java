ff467d09b22173e78e21d91b1487f7ed165f3024
Bruno Redzio <brunoredzio@hotmail.com>
2021-12-02 12:21:25 -0300

    Se creó registro y login

--- src/main/java/com/egg/patitas/red/model/User.java
+++ src/main/java/com/egg/patitas/red/model/User.java
@@ -2,14 +2,20 @@ package com.egg.patitas.red.model;
 
 import lombok.*;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.lang.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.*;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -20,33 +26,32 @@ import java.util.List;
 @Table(name = "user", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 @SQLDelete(sql = "UPDATE User SET enabled = false WHERE id = ?")
 @EntityListeners(AuditingEntityListener.class)
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @NotNull(message = "Name cannot be null")
+    @NotEmpty(message = "Nombre es obligatorio")
+    @NotNull(message = "Nombre no puede ser nulo")
     private String name;
 
-    @NotNull(message = "Lastname cannot be null")
+    @NotEmpty(message = "Apellido es obligatorio")
+    @NotNull(message = "Apellido no puede ser nulo")
     private String lastname;
 
-
-    @ManyToOne (fetch = FetchType.LAZY, cascade = CascadeType.ALL )
-    @JoinColumn(name = "roleId")
-    private Role role;
-
-    @NotNull(message = "Email cannot be null")
-    @Email(message = "Email should be valid.", regexp = "[A-Za-z0-9._%-+]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}")
-    @Size(min = 5, max = 15)
+    @NotEmpty(message = "Correo electrónico obligatorio")
+    @NotNull(message = "Correo electrónico no puede ser nulo")
+    @Email(message = "Correo electrónico ingresado inválido.", regexp = "[A-Za-z0-9._%-+]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}")
+    @Length(max = 80)
     private String email;
 
-    @NotNull(message = "Password cannot be null")
+    @NotEmpty(message = "Contraseña obligatoria")
+    @NotNull(message = "Contraseña no puede ser nula")
+    @Length(min = 5, message = "Ingrese una contraseña de minímo 5 carácteres")
     private String password;
 
-    @NotNull
-    private Boolean enabled = true;
+    private Boolean enabled;
 
     @CreatedDate
     @Column(updatable = false)
@@ -64,10 +69,39 @@ public class User {
     private List<Pet> pets;
 
     @ManyToOne
-    @JoinColumn(nullable=false)
-    private Role rol;
+    @JoinColumn(name="role")
+    private Role role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role.getName());
+        return Collections.singletonList(authority);
+    }
 
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
 
 
--- src/main/java/com/egg/patitas/red/service/UserService.java
+++ src/main/java/com/egg/patitas/red/service/UserService.java
@@ -2,47 +2,105 @@ package com.egg.patitas.red.service;
 
 import com.egg.patitas.red.config.RoleEnum;
 import com.egg.patitas.red.exception.EmailExistException;
+import com.egg.patitas.red.model.Role;
 import com.egg.patitas.red.model.User;
+import com.egg.patitas.red.repository.RoleRepository;
 import com.egg.patitas.red.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-import java.time.LocalDate;
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
-    private UserRepository userRepository;
+    RoleService roleService;
 
     @Autowired
-    private RoleService roleService;
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Transactional
-    public void createUser(User user)  throws EmailExistException {
-            if (userRepository.findByEmail(user.getEmail()) != null) {
-                throw new EmailExistException(user.getEmail());
+    public void createUser(User dto)  throws EmailExistException {
+       if (userRepository.findByEmail(dto.getEmail()) != null) {
+                throw new EmailExistException(dto.getEmail());
             }
-            userRepository.save(buildUser(user));
+
+        userRepository.save(buildUser(dto));
+
     }
 
-    private User buildUser(User user) {
-        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        user.setRole(roleService.findByName(RoleEnum.USER.getRoleName()));
+    @Transactional
+    public User buildUser(User dto) {
+        User user = new User();
+
+        user.setName(dto.getName());
+        user.setLastname(dto.getLastname());
+        user.setEmail(dto.getEmail());
+        user.setEnabled(true);
+        user.setPassword(bCryptPasswordEncoder.encode(dto.getPassword()));
+
+        if(roleRepository.findAll().isEmpty()){
+            Role roleUser = new Role();
+            Role roleAdmin = new Role();
+
+            roleUser.setName("USER");
+            roleAdmin.setName("ADMIN");
+
+            roleRepository.save(roleUser);
+            roleRepository.save(roleAdmin);
+        }
+
+        if(userRepository.findAll().isEmpty()){
+            user.setRole(roleService.findByName(RoleEnum.ADMIN.getName()));
+        }else{
+            user.setRole(roleService.findByName(RoleEnum.USER.getName()));
+        }
+
+        userRepository.save(user);
+        System.out.println(user.getId());
+
         return user;
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if(email == null){
+            throw new UsernameNotFoundException("Usuario no encontrado");
+        }
+
+        if(!user.getEnabled()){
+            throw new UsernameNotFoundException("Usuario dado de baja");
+        }
+
+        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER"+ user.getRole().getName());
+
+        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+
+        HttpSession sesion = attr.getRequest().getSession(true);
+
+        sesion.setAttribute("idUsuario", user.getId());
+        sesion.setAttribute("email", user.getEmail());
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(), Collections.singletonList(authority));
     }
 
     public List<User> findAll() {
