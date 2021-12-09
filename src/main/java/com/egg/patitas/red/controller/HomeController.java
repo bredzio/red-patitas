05aeb5506447bf0ea552e636fb75f1ba50ee5aea
Bruno Redzio <brunoredzio@hotmail.com>
2021-12-09 12:17:05 -0300

    Se hizo formulario de contacto

--- src/main/java/com/egg/patitas/red/controller/HomeController.java
+++ src/main/java/com/egg/patitas/red/controller/HomeController.java
@@ -1,21 +1,66 @@
 package com.egg.patitas.red.controller;
 
+import com.egg.patitas.red.model.Contact;
+import com.egg.patitas.red.model.User;
+import com.egg.patitas.red.service.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+    @Autowired
+    private ContactService contactService;
 
     @GetMapping("/home")
     public ModelAndView home(Principal principal) {
         ModelAndView mav = new ModelAndView("home");
         return mav;
     }
+
+    @PostMapping("/createContact")
+    public RedirectView createContact(@Valid @ModelAttribute Contact contact, BindingResult bindingResult, RedirectAttributes attributes, HttpServletRequest request) throws Exception {
+        RedirectView redirectView = new RedirectView("/");
+
+        if (bindingResult.hasErrors()) {
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            for (FieldError error : errors ) {
+                System.out.println(error.getDefaultMessage());
+                attributes.addFlashAttribute("error",error.getDefaultMessage());
+                attributes.addFlashAttribute("contact", contact);
+            }
+
+            return redirectView;
+        }
+
+        try{
+            contactService.createContact(contact);
+            attributes.addFlashAttribute("exito", "Mensaje enviado exitosamente");
+            attributes.addFlashAttribute("contact", contact);
+            redirectView.setUrl("/");
+        }catch(Exception e){
+            attributes.addFlashAttribute("error", e.getMessage());
+            attributes.addFlashAttribute("contact", contact);
+            redirectView.setUrl("/");
+        }
+
+        return redirectView;
+    }
+
 
     @GetMapping("/nosotros")
     public ModelAndView nosotros(Principal principal) {
--- src/main/java/com/egg/patitas/red/email/EmailService.java
+++ src/main/java/com/egg/patitas/red/email/EmailService.java
@@ -15,13 +15,13 @@ public class EmailService implements EmailSend{
 
     @Override
     @Async
-    public void send(String to, String email) {
+    public void send(String to, String email, String subject) {
         try {
             MimeMessage mimeMessage = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
             helper.setText(email, true);
             helper.setTo(to);
-            helper.setSubject("Confirme su email");
+            helper.setSubject(subject);
             helper.setFrom("huellaAPP@huellapp.com");
             mailSender.send(mimeMessage);
         } catch (MessagingException e) {
